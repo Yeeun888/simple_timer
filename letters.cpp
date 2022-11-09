9b698647dc0e43f5fbd48b7bffe6dda1a05ad018
Yeeun888 <108403458+Yeeun888@users.noreply.github.com>
2022-11-09 16:25:12 +0700

    finished counter prototype

---
+++ letters.cpp
@@ -0,0 +1,90 @@
+#include <array>
+#include <string>
+
+namespace Numbers {
+    using std::array;
+
+    array<std::string, 5> zero{
+        "██████",
+        "██  ██",
+        "██  ██",
+        "██  ██",
+        "██████",
+    };
+
+    array<std::string, 5> one{
+        "████  ",
+        "  ██  ",
+        "  ██  ",
+        "  ██  ",
+        "██████",
+    };
+
+    array<std::string, 5> two{
+        "██████",
+        "     █",
+        "██████",
+        "█     ",
+        "██████",
+    };
+
+    array<std::string, 5> three{
+        "██████"
+        "    ██"
+        "██████"
+        "    ██"
+        "██████"
+    };
+
+    array<std::string, 5> four{
+        "██  ██",
+        "██  ██",
+        "██████",
+        "    ██",
+        "    ██",
+    };
+
+    array<std::string, 5> five{
+        "██████",
+        "██    ",
+        "██████",
+        "    ██",
+        "██████",
+    };
+
+    array<std::string, 5> six{
+        "██████",
+        "██    ",
+        "██████",
+        "██  ██",
+        "██████",
+    };
+
+    array<std::string, 5> seven{
+        "██████",
+        "    ██",
+        "    ██",
+        "    ██",
+        "    ██",
+    };
+
+    array<std::string, 5> eight{
+        "██████",
+        "██  ██",
+        "██████",
+        "██  ██",
+        "██████",
+    };
+    
+    array<std::string, 5> nine{
+        "██████",
+        "██  ██",
+        "██████",
+        "    ██",
+        "██████",
+    };
+};
+
+namespace ExtraCharacters {
+    
+};
