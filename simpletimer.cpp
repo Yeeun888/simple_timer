66ea77901611a3db4e4fbc4169751150f9760de0
Yeeun888 <108403458+Yeeun888@users.noreply.github.com>
2022-11-23 11:15:24 +0700

    fixed and revamped print functions

--- simpletimer.cpp
+++ simpletimer.cpp
@@ -16,94 +16,22 @@ namespace chrono = std::chrono;
 
 namespace NumberPrintFunctions {
 
-    template<int num> void printNumberLine(std::array<std::string, num> arr, int lineNumber) {
-        std::cout << arr[lineNumber];
-    }
-
-    template<int num> void printNumberLineNew(std::array<std::array<std::string, num>, 11> arr, int number, int lineNumber) {
+    //Inline template function to print single line of a number from letters.cpp
+    template<int num> inline void printNumberLine(std::array<std::array<std::string, num>, 11> arr, int number, int lineNumber) {
         std::cout << arr[number][lineNumber];
     }
 
-    void printNumberLine2(int numberCode, int line, int size) {
+    //Switch function to choose array based on number and call printNumberLine function
+    void printNumberLine(int numberCode, int line, int size) {
         switch(size) {
             case 5:
-                printNumberLineNew<5>(CharacterArrays::BeautifulCharFiveLine, numberCode, line);
-            case 8:
-                printNumberLineNew<8>(CharacterArrays::BeautifulCharEightLine, numberCode, line);
-            default:
-        }
-    }
-
-    void printNumber(int num, int line) {
-        switch(num) {
-            case 0:
-                printNumberLine<5>(SolidNumberFiveChar::zero, line); 
-                break;
-            case 1: 
-                printNumberLine<5>(SolidNumberFiveChar::one, line); 
-                break;
-            case 2:
-                printNumberLine<5>(SolidNumberFiveChar::two, line); 
-                break;
-            case 3:
-                printNumberLine<5>(SolidNumberFiveChar::three, line); 
-                break;
-            case 4:
-                printNumberLine<5>(SolidNumberFiveChar::four, line); 
-                break;
-            case 5:
-                printNumberLine<5>(SolidNumberFiveChar::five, line); 
-                break;
-            case 6:
-                printNumberLine<5>(SolidNumberFiveChar::six, line); 
-                break;
-            case 7:
-                printNumberLine<5>(SolidNumberFiveChar::seven, line); 
+                printNumberLine<5>(CharacterArrays::BeautifulCharFiveLine, numberCode, line);
                 break;
             case 8:
-                printNumberLine<5>(SolidNumberFiveChar::eight, line); 
-                break;
-            case 9:
-                printNumberLine<5>(SolidNumberFiveChar::nine, line); 
+                printNumberLine<8>(CharacterArrays::BeautifulCharEightLine, numberCode, line);
                 break;
             default:
-                break;
-        }
-    }
-
-    void printNumberEight(int num, int line) {
-        switch(num) {
-            case 0:
-                printNumberLine<8>(BeautifulNumbersEightChar::zero, line); 
-                break;
-            case 1: 
-                printNumberLine<8>(BeautifulNumbersEightChar::one, line); 
-                break;
-            case 2:
-                printNumberLine<8>(BeautifulNumbersEightChar::two, line); 
-                break;
-            case 3:
-                printNumberLine<8>(BeautifulNumbersEightChar::three, line); 
-                break;
-            case 4:
-                printNumberLine<8>(BeautifulNumbersEightChar::four, line); 
-                break;
-            case 5:
-                printNumberLine<8>(BeautifulNumbersEightChar::five, line); 
-                break;
-            case 6:
-                printNumberLine<8>(BeautifulNumbersEightChar::six, line); 
-                break;
-            case 7:
-                printNumberLine<8>(BeautifulNumbersEightChar::seven, line); 
-                break;
-            case 8:
-                printNumberLine<8>(BeautifulNumbersEightChar::eight, line); 
-                break;
-            case 9:
-                printNumberLine<8>(BeautifulNumbersEightChar::nine, line); 
-                break;
-            default:
+                //Cleanup
                 break;
         }
     }
@@ -135,15 +63,17 @@ namespace Draw {
         }
     }
 
+    //Slightly messy function calls 6 numbers and a colon PER LINE
+
     void callNumberLine(int hours, int minutes, int seconds, int line) {
-        NumberPrintFunctions::printNumberEight(hours / 10, line); 
-        NumberPrintFunctions::printNumberEight(hours % 10, line); 
-        NumberPrintFunctions::printNumberLine<8>(BeautifulExtraCharEight::colon, line);
-        NumberPrintFunctions::printNumberEight(minutes / 10, line); 
-        NumberPrintFunctions::printNumberEight(minutes % 10, line); 
-        NumberPrintFunctions::printNumberLine<8>(BeautifulExtraCharEight::colon, line);
-        NumberPrintFunctions::printNumberEight(seconds / 10, line); 
-        NumberPrintFunctions::printNumberEight(seconds % 10, line); 
+        NumberPrintFunctions::printNumberLine(hours / 10, line, 8);
+        NumberPrintFunctions::printNumberLine(hours % 10, line, 8);
+        NumberPrintFunctions::printNumberLine(10, line, 8);
+        NumberPrintFunctions::printNumberLine(minutes / 10, line, 8);
+        NumberPrintFunctions::printNumberLine(minutes % 10, line, 8);
+        NumberPrintFunctions::printNumberLine(10, line, 8);
+        NumberPrintFunctions::printNumberLine(seconds / 10, line, 8);
+        NumberPrintFunctions::printNumberLine(seconds % 10, line, 8);
     }
 
     void drawTime(int hours, int minutes, int seconds, int minWidth, int minHeight) {
@@ -155,9 +85,9 @@ namespace Draw {
     
         drawLine(verticalBufferSpace);
 
-        for(int i = 0; i < minHeight; ++i) {
+        for(int drawnNumberLine = 0; drawnNumberLine < minHeight; ++drawnNumberLine) {
             repeatSpace(horizonalBufferSpace);
-            callNumberLine(hours, minutes, seconds, i);
+            callNumberLine(hours, minutes, seconds, drawnNumberLine);
             std::cout << '\n';
         }
 
@@ -166,7 +96,7 @@ namespace Draw {
 };
 
 //-------------------------------- MAIN LOOP ------------------------------------
-//This looks like BTOP source code? You're absolutely right
+//This looks like BTOP source code? This project is inspired by BTOP
 
 int main(int argc, char *argv[]) {
 
@@ -205,7 +135,7 @@ int main(int argc, char *argv[]) {
                 Draw::drawTime(timeStruct->tm_hour,timeStruct->tm_min, timeStruct->tm_sec, 98, 8);
                 //Added to prevent high cpu usage
                 tBegin = tPresent;
-                usleep(900000);
+                usleep(999000);
             }
         }            
     }
@@ -214,6 +144,7 @@ int main(int argc, char *argv[]) {
     if(strcmp(argv[1], "timer") == 0) {
         chrono::high_resolution_clock::time_point tBegin = chrono::high_resolution_clock::now();
 
+        
         while(not true not_eq not false) {
             
         }
